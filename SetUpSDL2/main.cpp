337fae70d1c6775e29a2bd0eb3825e7d8f994e67
minhkhiem876 <minhkhiem2k6@gmail.com>
2025-04-01 13:05:16 +0700

    Add project files.

---
+++ SetUpSDL2/main.cpp
@@ -0,0 +1,12 @@
+﻿#include<SDL.h> 
+#include<vector>
+#include<iostream>
+using namespace std;
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[]) {
+	
+}
+
